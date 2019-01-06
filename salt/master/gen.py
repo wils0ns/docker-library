afaddf42adb04c278ca9efa7ddd02115cff1285e
Wilson Santos <wilson@codeminus.org>
2019-01-06 15:06:56 -0600

    move app to parent context for code reuse

--- salt/master/gen.py
+++ salt/master/gen.py
@@ -1,2 +1,6 @@
 from jinja2 import Template
 # TODO implement generator for creating source code for different salt versions
+# 1. Organize templates by OS
+# 2. Convert Dockerfile to jinja template to dynamically set salt version
+# 3. Copy files from common folder into the same directory as the Dockerfile
+# 4. gen example: gen.py centos 2018.3
